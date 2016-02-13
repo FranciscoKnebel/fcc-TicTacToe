848e557deaf17091e6bec7df4b40039b833937c3
kyozeus <franciscopaivaknebel@gmail.com>
2016-02-13 16:52:38 -0200

    Added basic board, user team choice, working scoreboard.

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,79 +1,75 @@
-/*var app = angular.module('tictactoe', []);
-app.controller('RunescapeController', function($scope, $http) {
-  var callback = '&callback=JSON_CALLBACK';
+var chooseTeam = false;
+var currentTeam = '';
+var currentScore = [0, 0];
 
-  $scope.results = [];
-  $http.jsonp(apiURL + 'streams?game=RuneScape' + callback)
-  .success(function(data) {
-    var streams = data.streams;
-    $scope.results = buildStreams(streams);
-    console.log($scope.results[0]);
-  });
+var app = angular.module('tictactoe', ['ngAnimate']);
+app.controller('Game', function($scope) {
+  updateScore();
+
+  $scope.WinnerX = function() {
+    addWin('X');
+    updateScore();
+  }
+
+  $scope.WinnerO = function() {
+    addWin('O');
+    updateScore();
+  }
+
+  $scope.ChooseTeam = function(team) {
+    setUserTeam(team);
+  }
+
+  $scope.blockChoice = function(item) {
+    console.log(item);
+
+    var blockID = item.target.id;              /* Get ID from block */
+    var blockClass = item.target.classList[2]; /* Get class from block */
+
+    if(blockClass === 'empty') {
+      $("#" + blockID).removeClass('empty');
+      $("#" + blockID).addClass(currentTeam);
+    }
+    else if(blockClass === 'X' || blockClass === 'O') {
+      alert("You can't play there, dummy!");
+    }
+    else {
+      /*bad behavior*/
+      console.log("Error 002");
+    }
+
+
+  }
+
+
+  function addWin(Team) {
+    if(Team === 'X') {
+      currentScore[0]++;
+    }
+    else if(Team === 'O') {
+      currentScore[1]++;
+    }
+    else {
+      console.log("Error 001");
+    }
+  }
+
+  function updateScore() {
+    $scope.X = currentScore[0];
+    $scope.O = currentScore[1];
+
+    if(chooseTeam === false) {
+      $scope.choice = true;
+    }
+  }
+
+  function setUserTeam(team) {
+    if(team === 'X' || team === 'O') {
+      chooseTeam = true;
+      currentTeam = team;
+      $scope.choice = false;
+      console.log(currentTeam);
+    }
+  }
+
 });
-
-app.controller('userController', function($scope, $http) {
-  var callback = '?callback=JSON_CALLBACK';
-
-  $scope.results = [];
-  $scope.streaming = [];
-  $scope.notstreaming = [];
-  channels.forEach(function(channel) {
-    $http.jsonp(apiURL + 'streams/' + channel + callback)  //Check if channel is streaming
-    .success(function(data) {
-      var streaming;
-      var stream;
-      var channelData;
-
-      if(data.stream === undefined || data.stream === null) { // Channel is not streaming
-        streaming = false;
-        stream = null;
-      }
-      else {  // Channel is streaming
-        streaming = true;
-        stream = buildSingleStream(data.stream);
-      }
-
-      $http.jsonp(apiURL + 'channels/' + channel + callback) //Get channel information
-      .success(function(data) {
-        channelData = data;
-
-        if(streaming === true) {
-          $scope.streaming.push({streaming, stream, channelData});
-        }
-        else if (channelData.status === 422){
-          $scope.notstreaming.push({streaming, stream, channelData});
-
-        }
-        else {
-          $scope.notstreaming.push({streaming, stream, channelData});
-          console.log({streaming, stream, channelData});
-        }
-
-      });
-    });
-  });
-});
-
-function buildStreams(streams) {
-  var results = [];
-  angular.forEach(streams, function(stream) {
-    results.push(buildSingleStream(stream));
-  });
-
-  return results;
-}
-
-function buildSingleStream(stream) {
-  var result = {
-    display: stream.channel.display_name,
-    name: stream.channel.name,
-    logo: stream.channel.logo,
-    followers: stream.channel.followers,
-    url: stream.channel.url,
-    game: stream.game,
-    viewers: stream.viewers,
-    status: stream.channel.status
-  };
-
-  return result;
-}*/
