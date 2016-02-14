809d5042600d947379f0a4a0abbf2355f3ef13d7
kyozeus <franciscopaivaknebel@gmail.com>
2016-02-14 01:42:24 -0200

    Added AI with pseudorandom choices (just for testing).

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,6 +1,10 @@
 var chooseTeam = false;
 var currentTeam = '';
+var AITeam = '';
 var currentScore = [0, 0];
+var yourTurn = true;
+var currentMove = 0;
+
 
 var app = angular.module('tictactoe', ['ngAnimate']);
 app.controller('Game', function($scope) {
@@ -21,26 +25,45 @@ app.controller('Game', function($scope) {
   }
 
   $scope.blockChoice = function(item) {
-    console.log(item);
-
     var blockID = item.target.id;              /* Get ID from block */
     var blockClass = item.target.classList[2]; /* Get class from block */
 
-    if(blockClass === 'empty') {
-      $("#" + blockID).removeClass('empty');
-      $("#" + blockID).addClass(currentTeam);
-    }
-    else if(blockClass === 'X' || blockClass === 'O') {
-      alert("You can't play there, dummy!");
-    }
-    else {
-      /*bad behavior*/
-      console.log("Error 002");
-    }
+    if(yourTurn === true) {
+      if(blockClass === 'empty') {
+        //Sets block chosen and checks if user has a winning combo
+        setBlock("#" + blockID, true);
+        currentMove++;
+        checkWin(currentTeam);
+        yourTurn = false;
 
-
+        //now it is the AI's turn. Sets block and check if won
+        if(currentMove < 8) {
+          setBlock(AIchoice(), false);
+          currentMove++;
+          checkWin(AITeam);
+          yourTurn = true;
+        }
+      }
+      else if(blockClass === 'X' || blockClass === 'O') {
+        //alert("You can't play there, dummy!");
+      }
+      else {
+        /* bad behavior*/
+        console.log("Error 002");
+      }
+    }
   }
 
+  function setBlock(id, isPlayer) {
+    $(id).removeClass('empty');
+
+    if(isPlayer) {
+      $(id).addClass(currentTeam);
+    }
+    else {
+      $(id).addClass(AITeam);
+    }
+  }
 
   function addWin(Team) {
     if(Team === 'X') {
@@ -64,11 +87,39 @@ app.controller('Game', function($scope) {
   }
 
   function setUserTeam(team) {
-    if(team === 'X' || team === 'O') {
-      chooseTeam = true;
-      currentTeam = team;
-      $scope.choice = false;
-      console.log(currentTeam);
+    chooseTeam = true;
+    currentTeam = team;
+    $scope.choice = false;
+
+    if(team === 'X') {
+      AITeam = 'O'
+    }
+    else if (team === 'O') {
+      AITeam = 'X'
+    }
+    else { //bad behavior
+      chooseTeam = false;
+      $scope.choice = true;
+    }
+  }
+
+  function AIchoice() {
+    var flag = true;
+    var id;
+
+    while(flag) {
+      id = '#' + Math.floor((Math.random() * 8) + 0);
+
+      if ( $(id).hasClass('empty') ) {
+        flag = false;
+        return id;
+      }
+    }
+  }
+
+  function checkWin() {
+    for(var i = 0; i < 8; i++) {
+
     }
   }
 
